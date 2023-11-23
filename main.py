3a10c3e614787418b5cea9fac5a279be54da3980
PashkovD <dimon2006763@gmail.com>
2023-11-23 18:36:53 +0500

    Добавлено больше функциональности

--- main.py
+++ main.py
@@ -8,10 +8,52 @@ with open("token.txt") as f:
 bot = TeleBot(token)
 
 main_menu_markup = (ReplyKeyboardMarkup(one_time_keyboard=True)
-                    .add(types.KeyboardButton('Места сбора'))
+                    .add(types.KeyboardButton('Точки сбора'))
                     .add(types.KeyboardButton('О проекте'))
                     .add(types.KeyboardButton('Рейтинг школ'))
                     .add(types.KeyboardButton('Что можно сдавать в пункты приёма?')))
+cities: dict[str, dict[str, dict[str, str | float]]] = {
+    "Сургут": {
+        "г. Сургут УЛ. 30 ЛЕТ ПОБЕДЫ, Д. 74": {
+            "latitude": 61.2574603,
+            "longitude": 73.4570286,
+            "description": """
+ежедневно 10:00 - 20:00
+обед 14:00 - 15:00
+тех. перерывы
+11:45 - 12:00 / 16:45 - 17:00"""
+        }
+    },
+    "Ханты-Мансийск": {
+        "УЛ. 30 ЛЕТ ПОБЕДЫ, Д. 74": {
+            "latitude": 61.2574603,
+            "longitude": 73.4570286,
+            "description": """
+ежедневно 10:00 - 20:00
+обед 14:00 - 15:00
+тех. перерывы
+11:45 - 12:00 / 16:45 - 17:00"""
+        }
+    },
+    "Нижневартовск": {
+        "УЛ. 30 ЛЕТ ПОБЕДЫ, Д. 74": {
+            "latitude": 61.2574603,
+            "longitude": 73.4570286,
+            "description": """
+ежедневно 10:00 - 20:00
+обед 14:00 - 15:00
+тех. перерывы
+11:45 - 12:00 / 16:45 - 17:00"""
+        }
+    },
+}
+points_data: dict[str, dict[str, str | float]] = {}
+
+
+def load_points():
+    for city in cities.values():
+        for key in city.keys():
+            points_data[key] = city[key]
 
 
 @bot.message_handler(commands=["start"])
@@ -49,10 +91,13 @@ def rating(message: Message):
     )
 
 
-@bot.message_handler(func=(lambda message: message.text == "Места сбора"))
+@bot.message_handler(func=(lambda message: message.text == "Точки сбора"))
 def places(message: Message):
-    bot.send_message(message.chat.id, "Места сбора имеются в трёх городах.")
-    bot.send_message(message.chat.id, "В каком городе вы хотите найти точки сбора?", reply_markup=main_menu_markup)
+    bot.send_message(message.chat.id, f"Места сбора имеются в {len(cities)} городах")
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True).add("обратно")
+    for i in cities.keys():
+        markup.add(i)
+    bot.send_message(message.chat.id, "\n".join(cities.keys()), reply_markup=markup)
 
 
 @bot.message_handler(func=(lambda message: message.text == "Что можно сдавать в пункты приёма?"))
@@ -63,113 +108,33 @@ def what_to_take(message: Message):
                      reply_markup=main_menu_markup)
 
 
-@bot.message_handler()
+@bot.message_handler(func=(lambda message: message.text in cities.keys()))
 def trans(message: Message):
-    if message.text == 'В Ханты-Мансийске':
-        bot.send_message(message.chat.id, "УЛ. ЧЕХОВА, Д. 74")
-        bot.send_location(message.from_user.id, 61.0039543, 69.0526839)
-        bot.send_message(message.chat.id, """
-ежедневно 10:00 - 20:00
-обед 14:00 - 15:00
-тех. перерывы
-11:45 - 12:00 / 16:45 - 17:00""")
-        bot.send_message(message.chat.id, """
-ВИДЫ СЫРЬЯ И УСЛОВИЯ ПРИЁМА:
-    Коричневые, зеленые и бесцветные стеклянные бутылки и банки
-1 балл / кг. от 100 грамм ПРАВИЛА ПРИЁМА:
-Чистые, без крышек и ободков, бумажные этикетки можно не убирать
-Необходимо сдавать по отдельности коричневое, зеленое и бесцветное стекло
-Макулатура
-2 балл/кг от 100грамм
-ПРАВИЛА ПРИЁМА:
-    Книги, газеты, журналы, бумага, гофрокартон
-Чистое, сложенное в коробки или перевязанное, без жирных пятен и остатков еды, без файлов и скрепок
-
-НЕ ПРИНИМАЕТСЯ:
-    Одноразовая посуда, обои, чеки, пачки сигарет, мешки от строительных смесей, упаковка из пульперкартона, гофроупаковка от яиц
+    data: dict[str, dict[str, str | float]] = cities.get(message.text)
+    bot.send_message(message.chat.id, f"В {message.text} имеется {len(data)} точек сбора")
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True).add("обратно")
+    for i in data.keys():
+        markup.add(i)
+    bot.send_message(message.chat.id, "\n".join(data.keys()), reply_markup=markup)
 
 
-    Пластиковые ящики ПНД
-5 балл/кг от 100 грамм
-ПРАВИЛА ПРИЁМА:
-    Чистые, без остатков еды, жира и других загрязнений
-    Всех размеров, цветов и толщины""",
-                         reply_markup=main_menu_markup)
-    elif message.text == 'В Сургуте':
-        bot.send_message(message.chat.id, "УЛ. 30 ЛЕТ ПОБЕДЫ, Д. 74")
-        bot.send_location(message.from_user.id, 61.2574603, 73.4570286)
-        bot.send_message(message.chat.id, """
-ежедневно 10:00 - 20:00
-обед 14:00 - 15:00
-тех. перерывы
-11:45 - 12:00 / 16:45 - 17:00""")
-        bot.send_message(message.chat.id, """
-ВИДЫ СЫРЬЯ И УСЛОВИЯ ПРИЁМА:
-Коричневые, зеленые и бесцветные стеклянные бутылки и банки
-1 балл / кг. от 100 грамм
-ПРАВИЛА ПРИЁМА:
-Чистые, без крышек и ободков, бумажные этикетки можно не убирать
-Необходимо сдавать по отдельности коричневое, зеленое и бесцветное стекло
+@bot.message_handler(func=(lambda message: message.text == "обратно"))
+def places(message: Message):
+    bot.send_message(message.chat.id, """
+Доступные опции:
+    О проекте
+    Что можно сдавать
+    Точки сбора
+    Рейтинг школ""", reply_markup=main_menu_markup)
 
 
-Макулатура
-2 балл/кг от 100грамм
+@bot.message_handler(func=(lambda message: message.text in points_data.keys()))
+def points(message: Message):
+    data: dict[str, str | float] = points_data.get(message.text)
+    bot.send_message(message.chat.id, data["description"])
 
-ПРАВИЛА ПРИЁМА:
-
-Книги, газеты, журналы, бумага, гофрокартон
-
-Чистое, сложенное в коробки или перевязанное, без жирных пятен и остатков еды, без файлов и скрепок
-
-НЕ ПРИНИМАЕТСЯ:
-Одноразовая посуда, обои, чеки, пачки сигарет, мешки от строительных смесей, упаковка из пульперкартона, гофроупаковка от яиц
+    bot.send_location(message.chat.id, longitude=data["longitude"], latitude=data["latitude"])
 
 
-Пластиковые ящики ПНД
-5 балл/кг от 100 грамм
-ПРАВИЛА ПРИЁМА:
- 
-Чистые, без остатков еды, жира и других загрязнений
-
-Всех размеров, цветов и толщины""",
-                         reply_markup=main_menu_markup)
-    elif message.text == 'В Нижневартовске':
-        bot.send_message(message.chat.id, "УЛ. КОМСОМОЛЬСКОЕ ОЗЕРО, Д. 2")
-        bot.send_location(message.from_user.id, 61.2574603, 73.4570286)
-        bot.send_message(message.chat.id, """
-ежедневно 10:00 - 20:00
-обед 14:00 - 15:00
-тех. перерывы
-11:45 - 12:00 / 16:45 - 17:00""")
-        bot.send_message(message.chat.id, """
-ВИДЫ СЫРЬЯ И УСЛОВИЯ ПРИЁМА:
-Коричневые, зеленые и бесцветные стеклянные бутылки и банки
- 1 балл / кг. от 100 грамм
-ПРАВИЛА ПРИЁМА:
-
-Чистые, без крышек и ободков, бумажные этикетки можно не убирать
-
-Необходимо сдавать по отдельности коричневое, зеленое и бесцветное стекло
-
-
-Макулатура
- 2 балл/кг от 100грамм
- ПРАВИЛА ПРИЁМА:
- 
-Книги, газеты, журналы, бумага, гофрокартон
-
-Чистое, сложенное в коробки или перевязанное, без жирных пятен и остатков еды, без файлов и скрепок
-
-НЕ ПРИНИМАЕТСЯ:
-    Одноразовая посуда, обои, чеки, пачки сигарет, мешки от строительных смесей, упаковка из пульперкартона, гофроупаковка от яиц
-
-Пластиковые ящики ПНД
- 5 балл/кг от 100 грамм
-  ПРАВИЛА ПРИЁМА:
-  
-Чистые, без остатков еды, жира и других загрязнений
-
-Всех размеров, цветов и толщины""", reply_markup=main_menu_markup)
-
-
+load_points()
 bot.polling(non_stop=True)
